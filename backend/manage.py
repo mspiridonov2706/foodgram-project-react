2c0200930c20cb8bf05cdda40c34c2ddb69668b9
Mikhail Spiridonov <Muxa2793@mail.ru>
2022-04-24 15:47:15 +0300

    pep8

--- backend/manage.py
+++ backend/manage.py
@@ -1,10 +1,12 @@
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram_project.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'foodgram_project.settings',
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
